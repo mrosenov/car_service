65f7b69baea78daae2be8308f05b349bd0229e4b
mrosenov <mitkorosenov@live.com>
2022-10-28 17:05:48 +0300

    - added select2 lib for live search.

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -3,9 +3,13 @@
 
 <link href="{{asset('css/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
 <link href="{{asset('css/toastr/toastr.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/select2/select2.min.css')}}" rel="stylesheet">
+
 <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
 <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/sharp-solid.css">
 
 <script src="{{asset('js/jquery-3.6.1.min.js')}}"></script>
 <script src="{{asset('js/toastr/toastr.min.js')}}"></script>
+<script src="{{asset('js/select2/select2.min.js')}}"></script>
+
 <title>{{config('app.name')}}</title>
